5fc0011f0e2e9f0ddbee990cbbfa3019412f63da
Yash Garg <thisisyashgarg@gmail.com>
2023-02-01 17:30:37 +0530

    created routes to receive form data from frontend

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.json({ name: "yash" });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", cors(), (req, res) => {
+  res.send("Homepage");
+});
+
+app.post("/submitAdminForm", cors(), (req, res) => {
+  let { orgName, email } = req.body;
+  console.log(req.body);
+});
+
+app.post("/submitQID", cors(), (req, res) => {
+  const { qidValue } = req.body;
+  console.log(req.body);
 });
 
 app.listen(PORT, () => {
